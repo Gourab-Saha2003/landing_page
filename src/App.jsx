6cef33ef7ad9a53a729b0b7a914e5714c70e3223
Gourab Saha <gourabsaha2062003@gmail.com>
2023-10-16 03:11:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./Components/Home";
+
+const App=()=>{
+    return(
+        <>
+            <div className="App">
+                <Home />
+            </div>
+        </>
+    )
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <>
+        <div className='home-comtainer'>
+            <Navbar />
+        </div>
+    </>
+  )
+}
+
+export default Home;
