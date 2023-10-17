3353819050a74be073f0e8279efe9f9512300ebf
Gourab Saha <gourabsaha2062003@gmail.com>
2023-10-18 02:35:34 +0530

    landing page finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial.jsx";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
-const App=()=>{
-    return(
-        <>
-            <div className="App">
-                <Home />
-            </div>
-        </>
-    )
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
