c9653cdfe42e9d0d1a78a451a762eca14696a68a
Gourab Saha <gourabsaha2062003@gmail.com>
2023-10-20 00:25:49 +0530

    nothing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Logo from "../Assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
@@ -79,7 +79,7 @@ const Navbar = () => {
         </Box>
       </Drawer>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
